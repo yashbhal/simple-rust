4a1495f8f91d7e2caf746b7f38b0c2794d95afa3
Yash Bhalchandra <yash@Yashs-Laptop.local>
2024-03-22 19:01:54 -0700

    added function to convert fahrenheit to celcius

--- temperature_converter/src/main.rs
+++ temperature_converter/src/main.rs
@@ -15,7 +15,13 @@ fn main() {
     
     println!("The Fahrenheit temperature you entered is: {user_temp}");
 
-    let result_temp: f32 = ( (user_temp as f32 - 32.0) * 5.0) / 9.0;
+    let result_temp: f32 = fahrenheit_to_celcius(&user_temp);
+    // let result_temp: f32 = ( (user_temp as f32 - 32.0) * 5.0) / 9.0;
 
     println!("{user_temp} degrees Fahrenheit is equal to {result_temp} degrees Celsius."); 
+}
+
+fn fahrenheit_to_celcius(&fahrenheit_temp: &u32) -> f32{
+    let celcius_temp = ( (fahrenheit_temp as f32 - 32.0) * 5.0) / 9.0;
+    return celcius_temp;
 }
